642a44146da3feaaf03b7d77d6b9f65779672c8a
CuJo33 <77585826+CuJo33@users.noreply.github.com>
2021-09-20 10:58:52 +0100

    Updates

---
+++ precache-manifest.b9d97f975a102328f7a08d4d034c7a02.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72ab3165a78ad7310cf2beab0b81b2c1",
+    "url": "/FacePalm/index.html"
+  },
+  {
+    "revision": "01a723a36f8f0a8c84f1",
+    "url": "/FacePalm/static/css/2.0740d453.chunk.css"
+  },
+  {
+    "revision": "32661044abae7ebaff2a",
+    "url": "/FacePalm/static/css/main.c3780ddd.chunk.css"
+  },
+  {
+    "revision": "01a723a36f8f0a8c84f1",
+    "url": "/FacePalm/static/js/2.0c175703.chunk.js"
+  },
+  {
+    "revision": "d001b592baae1fe1a6ccd4eb5b4856b0",
+    "url": "/FacePalm/static/js/2.0c175703.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32661044abae7ebaff2a",
+    "url": "/FacePalm/static/js/main.e3176777.chunk.js"
+  },
+  {
+    "revision": "85025b54ce8b42ba11d9",
+    "url": "/FacePalm/static/js/runtime-main.ec3b79b7.js"
+  },
+  {
+    "revision": "27bc5802cdd975952afcff56d38d2f6b",
+    "url": "/FacePalm/static/media/facePalm.27bc5802.png"
+  },
+  {
+    "revision": "8ff3ad952e8955ffff8ad655ccd1a10a",
+    "url": "/FacePalm/static/media/fail.8ff3ad95.jpg"
+  },
+  {
+    "revision": "e0756cdb9d2baee4bc3f2880df89bc90",
+    "url": "/FacePalm/static/media/like.e0756cdb.png"
+  },
+  {
+    "revision": "f202b5ca46f7a579b7dda1d953ec1f4e",
+    "url": "/FacePalm/static/media/plate_background.f202b5ca.jpg"
+  }
+]);
